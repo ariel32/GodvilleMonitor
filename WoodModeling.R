6952732ca57f3a2237fb01482fa1cdd1fb18fa9d
ariel32 <argentum32@gmail.com>
2016-05-27 22:10:10 +0300

    DBupdate2

--- WoodModeling.R
+++ WoodModeling.R
@@ -1,5 +1,13 @@
 d = dir("Dungeons/", pattern = "*.csv")
-d = unlist(strsplit(d, ".d.csv"))
+
+for(x in d){
+  data = read.csv(paste0("Dungeons/", x), sep = ";")
+  god = gsub(pattern = "%20", replacement = " ", x = unlist(strsplit(x, ".d.csv")))
+  data = cbind(god, data)
+  write.table(data, "DungeonsDB.csv", sep = ";", col.names = F, append = T, row.names = F)
+}
+
+
 
 coeff <- sapply(d, FUN = function(x) {
   data = read.csv(paste0("Dungeons/", x), sep = ";")
@@ -7,5 +15,12 @@ coeff <- sapply(d, FUN = function(x) {
   }
 )
 
-  
-head(sort(coeff, decreasing = T))*60*60*24
+n = gsub(pattern = "%20", replacement = " ", x = unlist(strsplit(d, ".d.csv")))
+res = data.frame(name = n, woods = round(as.numeric(coeff*60*60*24),2))
+res <- res[order(-res$woods),]
+head(res)
+
+res$woods[res$name=="Capsula"]
+
+capsula.d = read.csv("capsula2.csv", sep = ";")
+capsula.woods = round(as.numeric(lm(wood_cnt ~ time, data = capsula.d)$coefficients[2])*60*60*24,2)
