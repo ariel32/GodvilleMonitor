eccfd338fe2d945d42c0fc791d091e7d5b2df600
ariel32 <argentum32@gmail.com>
2016-05-28 16:47:02 +0300

    update woodmodeling.r

--- WoodModeling.R
+++ WoodModeling.R
@@ -1,21 +1,13 @@
-d = dir("Dungeons/", pattern = "*.csv")
+d = read.csv("DungeonsDB.csv", sep = ";", stringsAsFactor = F)
+godnames = unique(d$godname)
 
-for(x in d){
-  data = read.csv(paste0("Dungeons/", x), sep = ";", encoding = "UTF-8")
-  god = gsub(pattern = "%20", replacement = " ", x = unlist(strsplit(x, ".d.csv")))
-  data = cbind(god, data)
-  write.table(data, "DungeonsDB.csv", sep = ";", col.names = F, append = T, row.names = F)
-}
-
-
-coeff <- sapply(d, FUN = function(x) {
-  data = read.csv(paste0("Dungeons/", x), sep = ";")
+coeff <- sapply(godnames, FUN = function(x) {
+  data = d[d$godname==x,]
   as.numeric(lm(wood_cnt ~ time, data = data)$coefficients[2])
   }
 )
 
-n = gsub(pattern = "%20", replacement = " ", x = unlist(strsplit(d, ".d.csv")))
-res = data.frame(name = n, woods = round(as.numeric(coeff*60*60*24),2))
+res = data.frame(name = godnames, woods = round(as.numeric(coeff*60*60*24),2))
 res <- res[order(-res$woods),]
 head(res)
 
