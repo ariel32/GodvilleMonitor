891ef68df87d84c43683976201a9fbdf7363132b
ariel32 <argentum32@gmail.com>
2016-06-02 14:35:19 +0300

    error checker added

--- WoodModeling.R
+++ WoodModeling.R
@@ -5,20 +5,45 @@ godnames = unique(d$godname)
 coeff <- sapply(godnames, FUN = function(x) {
   data = d[d$godname==x,]
   as.numeric(lm(wood_cnt ~ time, data = data)$coefficients[2])
-  }
-)
+  } )
 
 # переводим коэффициент наклона прямой в количество бревен
 res = data.frame(name = godnames, woods = round(as.numeric(coeff*60*60*24),2))
 res <- res[order(-res$woods),]
 head(res)
 
+# summarization
+res$active = ifelse(res$woods > median(res$woods, na.rm = T), 1, 0)
+res$alignment <- sapply(res$name, function(x) FUN = median(d$alignment[d$godname==x]))
+res$arena.wins <- sapply(res$name, function(x) FUN = median(d$arena.wins[d$godname==x]))
+res$arena.loses <- sapply(res$name, function(x) FUN = median(d$arena.loses[d$godname==x]))
+res$arena.rate <- sapply(res$name, function(x) FUN = median(d$arena.wins[d$godname==x])/median(d$arena.loses[d$godname==x]))
+res$gold <- sapply(res$name, function(x) FUN = median(d$gold_approx[d$godname==x]))
+res$level <- sapply(res$name, function(x) FUN = median(d$level[d$godname==x]))
+res$might         <- sapply(res$name, function(x) FUN = median(d$p.might[d$godname==x]))
+res$templehood    <- sapply(res$name, function(x) FUN = median(d$p.templehood[d$godname==x]))
+res$gladiatorship <- sapply(res$name, function(x) FUN = median(d$p.gladiatorship[d$godname==x]))
+res$mastery       <- sapply(res$name, function(x) FUN = median(d$p.mastery[d$godname==x]))
+res$taming        <- sapply(res$name, function(x) FUN = median(d$p.taming[d$godname==x]))
+res$survival      <- sapply(res$name, function(x) FUN = median(d$p.survival[d$godname==x]))
+res$savings       <- sapply(res$name, function(x) FUN = median(d$p.savings[d$godname==x]))
+res$align.r       <- sapply(res$name, function(x) FUN = median(d$p.alignment[d$godname==x]))
+
+res <- res[-which(is.nan(res$arena.rate) | is.infinite(res$arena.rate)),]
+
+l <- glm(active ~ alignment+arena.rate+gold+level+might+templehood+gladiatorship+mastery+taming+survival+savings+align.r, data = res, family=binomial(logit))
+summary(l)
+predict.glm(l, res[res$name=="Capsula",], type = "response")
+
+#####
 res$woods[res$name=="Capsula"]
 capsula.d = read.csv("capsula2.csv", sep = ";")
 capsula.woods = round(as.numeric(lm(wood_cnt ~ time, data = capsula.d)$coefficients[2])*60*60*24,2)
-plot(wood_cnt ~ time, data = d[d$godname=="Изерка",], type = "l")
+plot(wood_cnt ~ time, data = d[d$godname=="Capsula",], type = "l")
 
-
+######### ideal woodrate
+x = 1:100; y = seq(0, 396, by = 4)+cumsum(rep(c(rep(0,19),6),5))
+idealwoodrate = round(as.numeric(lm(y ~ x)$coefficients[2]),2)
 
 # получаем цифробуквенные данные и перезаписываем БД
 #####
