2b948dda3844184b356562c00669e9cb83884e30
ariel32 <argentum32@gmail.com>
2016-06-03 14:10:42 +0300

    achievments parsing added

--- WoodModeling.R
+++ WoodModeling.R
@@ -38,6 +38,7 @@ predict.glm(l, res[res$name=="Capsula",], type = "response")
 #####
 res$woods[res$name=="Capsula"]
 capsula.d = read.csv("capsula2.csv", sep = ";")
+plot(wood_cnt ~ time, data = capsula.d, type = "l")
 capsula.woods = round(as.numeric(lm(wood_cnt ~ time, data = capsula.d)$coefficients[2])*60*60*24,2)
 plot(wood_cnt ~ time, data = d[d$godname=="Capsula",], type = "l")
 
@@ -45,6 +46,32 @@ plot(wood_cnt ~ time, data = d[d$godname=="Capsula",], type = "l")
 x = 1:100; y = seq(0, 396, by = 4)+cumsum(rep(c(rep(0,19),6),5))
 idealwoodrate = round(as.numeric(lm(y ~ x)$coefficients[2]),2)
 
+#### EXTRACT ACHIEVMENTS
+god = "GodWin"
+html = GET(sprintf("http://godville.net/gods/%s",god))
+if(length(readHTMLList(htmlParse(html))) > 1) a <- gsub(pattern = "\n\t\t\t\t\n\t\t\t\t\t\n\t\t\t\t\t\t", " ", readHTMLList(htmlParse(html))[[2]])
+a <- data.frame(achievment = sapply(strsplit(a, ","), "[[", 1), value = as.numeric(gsub("[^0-9]", "", sapply(strsplit(a, ","), "[", 2))), stringsAsFactors = F)
+a$value[grep("Заслуженный", a$achievment)] <- 0
+a$achievment <- gsub("Заслуженный ", "", a$achievment)
+a.lamb        <- a$value[a$achievment=="Агнец"]
+a.imp         <- a$value[a$achievment=="Чертяка"]
+a.martyr      <- a$value[a$achievment=="Мученик"]
+a.favorite    <- a$value[a$achievment=="Фаворит"]
+a.scoffer     <- a$value[a$achievment=="Безбожник"]
+a.warrior     <- a$value[a$achievment=="Вояка"]
+a.maniac      <- a$value[a$achievment=="Маньяк"]
+a.champion    <- a$value[a$achievment=="Чемпион"]
+a.tutor       <- a$value[a$achievment=="Наставник"]
+a.hunter      <- a$value[a$achievment=="Охотник"]
+a.plunderer   <- a$value[a$achievment=="Расхититель"]
+a.careerist   <- a$value[a$achievment=="Карьерист"]
+a.breeder     <- a$value[a$achievment=="Заводчик"]
+a.architect   <- a$value[a$achievment=="Зодчий"]
+a.shipbuilder <- a$value[a$achievment=="Корабел"]
+a.sailor      <- a$value[a$achievment=="Мореход"]
+a.fowler      <- a$value[a$achievment=="Ловец"]
+achievment <- sapply(list(a.lamb,a.imp,a.martyr,a.favorite,a.scoffer,a.warrior,a.maniac,a.champion,a.tutor,a.hunter,a.plunderer,a.careerist,a.breeder,a.architect,a.shipbuilder,a.sailor,a.fowler), function(x) ifelse(length(x) > 0,x,4))
+names(achievment) <- c("a.lamb","a.imp","a.martyr","a.favorite","a.scoffer","a.warrior","a.maniac","a.champion","a.tutor","a.hunter","a.plunderer","a.careerist","a.breeder","a.architect","a.shipbuilder","a.sailor","a.fowler")
 # получаем цифробуквенные данные и перезаписываем БД
 #####
 monsters_killed[grep("ни одного", d$monsters_killed)] <- 0
