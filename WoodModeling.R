e7be2a58e402f4e86634002974b35c6d8398f990
ariel32 <argentum32@gmail.com>
2016-07-10 13:46:41 +0300

    before clean up code

--- WoodModeling.R
+++ WoodModeling.R
@@ -1,66 +1,128 @@
-############################ DATA TRANSFORMING AND PREPEARING
-#############################################################
-d = read.csv("DungeonsDB.csv", sep = ";", stringsAsFactor = F)
-godnames = unique(d$godname)
-#save(godnames, file = "godnames")
+library("jsonlite"); library("XML"); library("httr"); library("xgboost"); library("ggplot2"); library("caret"); library("ROCR")
+monitor <- function(god, res.return = FALSE) {
+  print(god)
+  js = GET(sprintf("http://godville.net/gods/api/%s.json", god))
+  html = GET(sprintf("http://godville.net/gods/%s",god))
+  if (js$status_code == 200 & html$status_code == 200) {
+    js <- content(js)
+    # парсим ачивки
+    if(length(readHTMLList(htmlParse(html))) > 1) a <- gsub(pattern = "\n\t\t\t\t\n\t\t\t\t\t\n\t\t\t\t\t\t", " ", readHTMLList(htmlParse(html))[[2]])
+    html = readHTMLTable(htmlParse(html), header = F, stringsAsFactors = FALSE, encoding = "UTF-8")
+    if(!is.null(html$characteristics) & !is.null(html$panteons)) {
+      godname         = js$godname
+      time            = as.numeric(as.POSIXct(Sys.time()))
+      # gold conversion
+      k = as.numeric(gsub("[^0-9]", "", js$gold_approx))
+      if(is.na(k)) k = 1
+      if(length(grep("ни одного", js$gold_approx))) gold_approx <- 0
+      if(length(grep("неизвестно", js$gold_approx))) gold_approx <- NA
+      if(length(grep("дес", js$gold_approx))) gold_approx <- k*10
+      if(length(grep("сот", js$gold_approx))) gold_approx <- k*100
+      if(length(grep("тыс", js$gold_approx))) gold_approx <- k*1000
+      if(length(grep("несколько", js$gold_approx))) gold_approx <- 5
+      level           = js$level
+      equip.level     = median(as.numeric(html$equipment$V3), na.rm = T)
+      if(js$alignment=="нейтральный") alignment <- 0
+      if(js$alignment=="недовольный") alignment <- -1
+      if(js$alignment=="озлобленный") alignment <- -2
+      if(js$alignment=="агрессивный") alignment <- -3
+      if(js$alignment=="злобный")     alignment <- -4
+      if(js$alignment=="чистое зло")  alignment <- -5
+      if(js$alignment=="чистое зло!") alignment <- -6
+      if(js$alignment=="беззлобный")        alignment <- 1
+      if(js$alignment=="добродушный")       alignment <- 2
+      if(js$alignment=="миролюбивый")       alignment <- 3
+      if(js$alignment=="добродетельный")    alignment <- 4
+      if(js$alignment=="абсолютное добро")  alignment <- 5
+      if(js$alignment=="абсолютное добро!") alignment <- 6
+      wood_cnt        = js$wood_cnt
+      health          = js$max_health
+      pet_level       = ifelse(is.null(js$pet$pet_level), "NA", js$pet$pet_level)
+      # age conversion
+      age             = html$characteristics$V2[2]
+      age <- unlist(strsplit(age, "[^[:digit:]]"))
+      age <- as.numeric(age[age != ""])
+      if(length(age)==1) {if(grepl("мес", age)) {age = age*30} else {age = age*360}}
+      if(length(age)==2) age <- age[1]*360 + age[2]*30
+      # killed monsters conversion
+      monsters_killed = html$characteristics$V2[6]
+      k = as.numeric(gsub("[^0-9]", "", monsters_killed))
+      if(is.na(k)) k = 1
+      if(length(grep("ни одного", monsters_killed))) monsters_killed <- 0
+      if(length(grep("дес", monsters_killed))) monsters_killed <- k*10
+      if(length(grep("сот", monsters_killed))) monsters_killed <- k*100
+      if(length(grep("тыс", monsters_killed))) monsters_killed <- k*1000
+      deaths          = html$characteristics$V2[7]
+      arena.wins      = as.numeric(strsplit(html$characteristics$V2[8], split = " / ")[[1]][1])
+      arena.loses     = as.numeric(strsplit(html$characteristics$V2[8], split = " / ")[[1]][2])
+      p.might         = html$panteons$V2[html$panteons$V1 == "Мощи"]
+      p.templehood    = html$panteons$V2[html$panteons$V1 == "Храмовничества"]
+      p.gladiatorship = ifelse(length(html$panteons$V2[html$panteons$V1 == "Гладиаторства"]) == 0, 0, html$panteons$V2[html$panteons$V1 == "Гладиаторства"])
+      p.mastery       = html$panteons$V2[html$panteons$V1 == "Мастерства"]
+      p.taming        = ifelse(length(html$panteons$V2[html$panteons$V1 == "Звероводства"]) == 0, 0, html$panteons$V2[html$panteons$V1 == "Звероводства"])
+      p.survival      = html$panteons$V2[html$panteons$V1 == "Живучести"]; if(identical(p.survival,character(0))) p.survival = NA
+      p.savings       = html$panteons$V2[html$panteons$V1 == "Зажиточности"]
+      p.alignment     = ifelse(length(html$panteons$V2[html$panteons$V1 == "Созидания" | html$panteons$V1 == "Разрушения"]) == 0, 0, html$panteons$V2[html$panteons$V1 == "Созидания" | html$panteons$V1 == "Разрушения"])
+      ##### achievments
+      a <- data.frame(achievment = sapply(strsplit(a, ","), "[[", 1), value = as.numeric(gsub("[^0-9]", "", sapply(strsplit(a, ","), "[", 2))), stringsAsFactors = F)
+      a$value[grep("Заслуженный", a$achievment)] <- 0
+      a$achievment <- gsub("Заслуженный ", "", a$achievment)
+      a.lamb        <- a$value[a$achievment=="Агнец"]
+      a.imp         <- a$value[a$achievment=="Чертяка"]
+      a.martyr      <- a$value[a$achievment=="Мученик"]
+      a.favorite    <- a$value[a$achievment=="Фаворит"]
+      a.scoffer     <- a$value[a$achievment=="Безбожник"]
+      a.warrior     <- a$value[a$achievment=="Вояка"]
+      a.maniac      <- a$value[a$achievment=="Маньяк"]
+      a.champion    <- a$value[a$achievment=="Чемпион"]
+      a.tutor       <- a$value[a$achievment=="Наставник"]
+      a.hunter      <- a$value[a$achievment=="Охотник"]
+      a.plunderer   <- a$value[a$achievment=="Расхититель"]
+      a.careerist   <- a$value[a$achievment=="Карьерист"]
+      a.breeder     <- a$value[a$achievment=="Заводчик"]
+      a.architect   <- a$value[a$achievment=="Зодчий"]
+      a.shipbuilder <- a$value[a$achievment=="Корабел"]
+      a.sailor      <- a$value[a$achievment=="Мореход"]
+      a.fowler      <- a$value[a$achievment=="Ловец"]
+      achievment <- sapply(list(a.lamb,a.imp,a.martyr,a.favorite,a.scoffer,a.warrior,a.maniac,a.champion,a.tutor,a.hunter,a.plunderer,a.careerist,a.breeder,a.architect,a.shipbuilder,a.sailor,a.fowler), function(x) ifelse(length(x) > 0,x,4))
+      names(achievment) <- c("a.lamb","a.imp","a.martyr","a.favorite","a.scoffer","a.warrior","a.maniac","a.champion","a.tutor","a.hunter","a.plunderer","a.careerist","a.breeder","a.architect","a.shipbuilder","a.sailor","a.fowler")
+      achievment <- as.data.frame(t(achievment))
+      a <- cbind.data.frame(godname,time,gold_approx,level,equip.level,alignment,wood_cnt,health,pet_level,age,monsters_killed,
+                            deaths,arena.wins,arena.loses,p.might,p.templehood,p.gladiatorship,p.mastery,p.taming,p.survival,p.savings,p.alignment)
+      a <- cbind(a,achievment)
+      #### return result if we just monitoring one god
+      if(res.return == TRUE) return(data.frame(alignment = a$alignment, arena.wins = a$arena.wins, arena.loses = a$arena.loses,
+                                               arena.rate = a$arena.wins/a$arena.loses, gold_approx = a$gold_approx, level = a$level, age = a$age,
+                                               equip.rate = a$equip.level/a$level, might = a$p.might, templehood = a$p.templehood,
+                                               gladiatorship = a$p.gladiatorship, mastery = a$p.mastery, taming = a$p.taming, survival = a$p.survival,
+                                               savings = a$p.savings, alignment.p = a$p.alignment, a.lamb = a$a.lamb, a.imp = a$a.imp, a.martyr = a$a.martyr,
+                                               a.favorite = a$a.favorite, a.scoffer = a$a.scoffer, a.warrior = a$a.warrior, a.maniac = a$a.maniac, a.champion = a$a.champion,
+                                               a.tutor = a$a.tutor, a.hunter = a$a.hunter, a.plunderer = a$a.plunderer, a.careerist = a$a.careerist, a.breeder = a$a.breeder,
+                                               a.shipbuilder = a$a.shipbuilder, a.sailor = a$a.sailor))
+      if(file.exists("DungeonsDB.csv")) {
+        write.table(a, "DungeonsDB.csv", sep = ";", col.names = F, append = T, row.names = F)
+      } else {
+        write.table(a, "DungeonsDB.csv", sep = ";", col.names = T, row.names = F)
+      }
+    }
+  } else { print("Status not OK"); write.table(data.frame(time = Sys.time(), godname = god, js.status = js$status_code, html.status = html$status_code), file = "log.csv", sep = ";", row.names = F, append = T, col.names = F)}
+  Sys.sleep(5)
+}
+source("res.function.R")
+res <- res.function()
 
-coeff <- sapply(godnames, FUN = function(x) {
-  data = d[d$godname==x,]
-  as.numeric(lm(wood_cnt ~ time, data = data)$coefficients[2])
-  } )
+###################################################
+# XGBOOST
+res[is.na(res)] <- -999
+label = res$active
+res <- res[,4:34]
 
-# переводим коэффициент наклона прямой в количество бревен
-res = data.frame(name = godnames, woods = round(as.numeric(coeff*60*60*24),2), stringsAsFactors = F)
-res <- res[order(-res$woods),]
-head(res)
+xgb.data.matrix <- xgb.DMatrix(data = data.matrix(res), label = label, missing = -999)
 
-# summarization
-res$active = ifelse(res$woods > median(res$woods, na.rm = T), 1, 0)
-res$alignment <- sapply(res$name, function(x) FUN = median(d$alignment[d$godname==x]))
-res$arena.wins <- sapply(res$name, function(x) FUN = median(d$arena.wins[d$godname==x]))
-res$arena.loses <- sapply(res$name, function(x) FUN = median(d$arena.loses[d$godname==x]))
-res$arena.rate <- sapply(res$name, function(x) FUN = median(d$arena.wins[d$godname==x])/median(d$arena.loses[d$godname==x]))
-res$gold <- sapply(res$name, function(x) FUN = median(d$gold_approx[d$godname==x]))
-res$level <- sapply(res$name, function(x) FUN = median(d$level[d$godname==x]))
-res$equip.rate    <- sapply(res$name, function(x) FUN = median(d$equip.level[d$godname==x], na.rm = T)/median(d$level[d$godname==x], na.rm = T))
-res$might         <- sapply(res$name, function(x) FUN = median(d$p.might[d$godname==x]))
-res$templehood    <- sapply(res$name, function(x) FUN = median(d$p.templehood[d$godname==x]))
-res$gladiatorship <- sapply(res$name, function(x) FUN = median(d$p.gladiatorship[d$godname==x]))
-res$mastery       <- sapply(res$name, function(x) FUN = median(d$p.mastery[d$godname==x]))
-res$taming        <- sapply(res$name, function(x) FUN = median(d$p.taming[d$godname==x]))
-res$survival      <- sapply(res$name, function(x) FUN = median(d$p.survival[d$godname==x]))
-res$savings       <- sapply(res$name, function(x) FUN = median(d$p.savings[d$godname==x]))
-res$align.r       <- sapply(res$name, function(x) FUN = median(d$p.alignment[d$godname==x]))
-res$a.lamb        <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.lamb[d$godname==x], na.rm = T)))
-res$a.imp         <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.imp[d$godname==x], na.rm = T)))
-res$a.martyr      <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.martyr[d$godname==x], na.rm = T)))
-res$a.favorite    <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.favorite[d$godname==x], na.rm = T)))
-res$a.scoffer     <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.scoffer[d$godname==x], na.rm = T)))
-res$a.warrior     <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.warrior[d$godname==x], na.rm = T)))
-res$a.maniac      <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.maniac[d$godname==x], na.rm = T)))
-res$a.champion    <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.champion[d$godname==x], na.rm = T)))
-res$a.tutor       <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.tutor[d$godname==x], na.rm = T)))
-res$a.hunter      <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.hunter[d$godname==x], na.rm = T)))
-res$a.plunderer   <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.plunderer[d$godname==x], na.rm = T)))
-res$a.careerist   <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.careerist[d$godname==x], na.rm = T)))
-res$a.breeder     <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.breeder[d$godname==x], na.rm = T)))
-#res$a.architect   <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.architect[d$godname==x], na.rm = T)))
-res$a.shipbuilder <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.shipbuilder[d$godname==x], na.rm = T)))
-res$a.sailor      <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.sailor[d$godname==x], na.rm = T)))
-#res$a.fowler      <- sapply(res$name, function(x) FUN = as.numeric(median(d$a.fowler[d$godname==x], na.rm = T)))
-
-
-############# DATA CLEANING
-#res <- res[complete.cases(res),]
-#res <- res[-which(is.nan(res$arena.rate) | is.infinite(res$arena.rate)),]
-
-########## XGBOOST
-library(xgboost); library(corrplot); library(Rtsne); library(ggplot2); library(caret)
-dm <- xgb.DMatrix(data = data.matrix(res[,4:33]), label = res[,3], missing = NA)
 param <- list("objective" = "binary:logistic",    # binary classification 
               "num_class" = 1,    # number of classes 
               "eval_metric" = "auc",    # evaluation metric 
-              "nthread" = 8,   # number of threads to be used 
+              "nthread" = 16,   # number of threads to be used 
               "max_depth" = 16,    # maximum depth of tree 
               "eta" = 1,    # step size shrinkage 
               "gamma" = 0,    # minimum loss reduction 
@@ -68,31 +130,29 @@ param <- list("objective" = "binary:logistic",    # binary classification
               "colsample_bytree" = 1,  # subsample ratio of columns when constructing each tree 
               "min_child_weight" = 12  # minimum sum of instance weight needed in a child 
 )
-bst = xgboost(data = dm, params = param, nrounds = 80)
-xgb.plot.tree(feature_names = names(res[,4:33]),model = bst)
+m.xgboost = xgboost(data = xgb.data.matrix, params = param, nrounds = 5000)
 
-importance_matrix <- xgb.importance(names(res[,4:33]), model = bst)
-xgb.plot.importance(importance_matrix)
+#xgb.plot.tree(feature_names = names(res[,4:33]),model = m.xgboost)
+#importance_matrix <- xgb.importance(names(res[,4:33]), model = m.xgboost)
+#xgb.plot.importance(importance_matrix)
 
+newdata <- monitor("Nekonekoneko", res.return = TRUE)
+capsula <- monitor("Capsula", res.return = TRUE)
 
-predict(bst, newdata = data.matrix(res[which(res$name=="Capsula"),4:33]))
+predict(m.xgboost, newdata = data.matrix(newdata))
+predict(m.xgboost, newdata = data.matrix(capsula))
 
-pred = predict(bst, dm)
+#pred = predict(m.xgboost, data.matrix(res[,4:33]))
 
-cv.res <- xgb.cv(data = dm, nfold = 5, label = res$active, nrounds = 20,
-                 objective = "binary:logistic", eval_metric = "auc", prediction=TRUE)
-cv.res
-
-confusionMatrix(data=cv.res, res$active)
+cv.res <- xgb.cv(data = xgb.data.matrix, nfold = 8, label = label, nrounds = 100, objective = "binary:logistic", eval_metric = "auc", prediction=TRUE)
+cv.res$pred
+# confusion matrix
+cv.res$pred <- ifelse(as.numeric(cv.res$pred) > 0.5, 1, 0)
+confusionMatrix(data=cv.res$pred, label)
 
 ################### ROC
-library(ROCR)
-library(MKmisc)
-library(caret); library(e1071)
-library(ggplot2); library(DAAG)
-
-predicted <- predict(bst, newdata = dm)
-prob <- prediction(predicted, res$active)
+predicted <- predict(m.xgboost, newdata = xgb.data.matrix)
+prob <- prediction(predicted, label)
 tprfpr <- performance(prob, "tpr", "fpr")
 tpr <- unlist(slot(tprfpr, "y.values"))
 fpr <- unlist(slot(tprfpr, "x.values"))
@@ -115,14 +175,18 @@ ggplot(roc) + geom_line(aes(x = fpr, y = tpr), size = 1.8, color = "green") +
   theme(plot.title = element_text(size = rel(1.5)),
         axis.title = element_text(size = rel(1.5)))
 
-# CV of models
-CVbinary(bst) # as optimal choosed first model
-
 ##### k-fold
-ctrl <- trainControl(method = "repeatedcv", number = 10, savePredictions = TRUE)
-mod_fit <- train(t.formula,  data=data.frame(pc.predict), method="glm", family="binomial", trControl = ctrl, tuneLength = 5)
-pred = predict(mod_fit, newdata=data.frame(pc.predict)) #for good testing needed replace d for testing set :(
-confusionMatrix(data=pred, res$active)
+xgb_grid = expand.grid(nrounds = 100,eta = 1,max_depth = c(2, 4, 6, 8, 10),gamma = c(0,1),colsample_bytree=1,min_child_weight=12)
+xgb_ctrl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
+m.xgboost.cv <- train(x = res, y = as.factor(label), method="xgbTree", trControl = xgb_ctrl, tuneGrid = xgb_grid)
+pred = predict(m.xgboost.cv, newdata=res) #for good testing needed replace d for testing set :(
+confusionMatrix(data=pred, label)
+
+predict(m.xgboost, newdata = data.matrix(newdata))
+predict(m.xgboost, newdata = data.matrix(capsula))
+
+
+
 
 corrplot.mixed(cor(data[,2:31]), lower="circle", upper="color", tl.pos="lt", diag="n", order="hclust", hclust.method="complete")
 
@@ -406,5 +470,15 @@ monsters_killed[grep("тыс", d$monsters_killed)] <- 1000
 i <- which(!is.na(as.numeric(gsub("[^0-9]", "", d$monsters_killed))))
 monsters_killed[i] <- monsters_killed[i]*as.numeric(gsub("[^0-9]", "", d$monsters_killed))[i]
 d$monsters_killed <- monsters_killed
+########## age
+for(x in 1:length(d$age)) {
+  age <- unlist(strsplit(d$age[x], "[^[:digit:]]"))
+  age <- as.numeric(age[age != ""])
+  if(length(age)==1) {if(grepl("мес", d$age[x])) {age = age*30} else {age = age*360}}
+  if(length(age)==2) age <- age[1]*360 + age[2]*30
+  d$age[x] <- age
+}
+
+
 write.table(d, "DungeonsDB.csv", sep = ";", col.names = T, row.names = F)
 #####
